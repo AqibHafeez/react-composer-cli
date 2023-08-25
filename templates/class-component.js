8871e49305be365c24244ebcde56d419eac876ae
Aqib <aqibhafeez47@gmail.com>
2023-08-25 23:50:25 +0500

    #1.0.1 added support for lifecyles methods/hooks

--- templates/class-component.js
+++ templates/class-component.js
@@ -2,6 +2,20 @@ const generateClassComponent = (componentName) => `
 import React, { Component } from 'react';
 
 class ${componentName} extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Initialize your state properties here
+    };
+  }
+  componentDidMount() {
+    // Perform any initialization, API calls, or subscriptions here
+  }
+  
+  componentWillUnmount() {
+    // Clean up resources, cancel subscriptions, etc. here
+  }
+
   render() {
     return (
       <>
